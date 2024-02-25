8ad2554bf061e86649868829d09a2e2729303193
amirhosein06 <amirhoseinzolfaghari06@gmail.com>
2024-02-25 11:01:19 +0330

    add base

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function playandstop() {
+    const audiotag = document.querySelectorAll("audio");
+    audiotag[0].play();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="play_list"></div>
+      <div className="player">
+        <div className="pl_btn"> open play list</div>
+        <div className="cover"><img src="..." alt="music cover" /></div>
+      <audio controls autoplay>
+      <source src="assets/Afshin Moghadam-Zemestoon.mp3" type="audio/mpeg"/>
+      </audio>
+      <div className="information">
+        <span className="name">mame</span>
+        <span className="artist">artist</span>
+      </div>
+      <input type="range" className="timline" value="0"/>
+      <div className="controls">
+      <button className="randombtn">r</button>
+      <button className="bacbtn">b</button>
+        <button className="playbtn" onClick={playandstop}>p</button>
+        <button className="nextbtn">n</button>
+        <button className="starbtn">s</button>
+
+      </div>
+      </div>
     </div>
   );
 }
