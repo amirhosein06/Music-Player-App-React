ee4eeaff92b098bbd4e5ee3294e44aa032015eaa
amirhosein06 <amirhoseinzolfaghari06@gmail.com>
2024-02-25 14:44:24 +0330

    add icon

--- src/App.js
+++ src/App.js
@@ -8,24 +8,29 @@ function App() {
   }
   return (
     <div className="App">
+
       <div className="play_list"></div>
+
       <div className="player">
-        <div className="pl_btn"> open play list</div>
+        <div className="pl_btn"><img src="icon/chevron-down-solid.svg"/>Open play list<img src="icon/ellipsis-solid.svg"/></div>
+
         <div className="cover"><img src="..." alt="music cover" /></div>
       <audio controls autoplay>
       <source src="assets/Afshin Moghadam-Zemestoon.mp3" type="audio/mpeg"/>
       </audio>
+
       <div className="information">
         <span className="name">mame</span>
         <span className="artist">artist</span>
       </div>
+
       <input type="range" className="timline" value="0"/>
       <div className="controls">
-      <button className="randombtn">r</button>
-      <button className="bacbtn">b</button>
-        <button className="playbtn" onClick={playandstop}>p</button>
-        <button className="nextbtn">n</button>
-        <button className="starbtn">s</button>
+      <button className="randombtn"><img src="icon/shuffle-solid.svg"/></button>
+      <button className="bacbtn"><img src="icon/backward-solid.svg"/></button>
+        <button className="playbtn" onClick={playandstop}><img src="icon/play-solid.svg"/></button>
+        <button className="nextbtn"><img src="icon/forward-solid.svg"/></button>
+        <button className="starbtn"><img src="icon/thumbs-up-regular.svg"/></button>
 
       </div>
       </div>
---
+++ src/data.js
@@ -0,0 +1,8 @@
+export default data = [
+    {
+        src:"assets/Afshin Moghadam-Zemestoon.mp3",
+        name:"Zemestoon",
+        artist:"Afshin Moghadam",
+        cover:""
+    }
+]
